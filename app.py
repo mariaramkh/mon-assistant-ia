7d2bac42c61f4e3e43831c5eb2ccf831fd5577b6
mariaramkh <mkhendriche@gmail.com>
2025-04-28 16:46:53 +0200

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+import os
 import streamlit as st
 
+# Obtenir le bon port
+port = int(os.environ.get("PORT", 8501))
+
+# Redirection vers la page d'accueil
 st.switch_page("pages/0_Accueil.py")
+
+# Lancer Streamlit correctement
+if __name__ == "__main__":
+    import streamlit.web.cli as stcli
+    import sys
+    sys.argv = ["streamlit", "run", "app.py", "--server.port", str(port)]
+    sys.exit(stcli.main())
